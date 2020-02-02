3fb55aefab0100b8777d598c7501f6f6377e8297
Nikolas Lada <nikolas.lada@gmail.com>
2020-02-02 23:34:28 +0100

    Added tests/bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) 2020 Nikolas Lada <https://nikolaslada.cz>.
+ */
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new \Nette\Configurator;
+
+$configurator->setDebugMode(true);
+
+$configurator->setTimeZone('Europe/Prague');
+$configurator->setTempDirectory(__DIR__ . '/tmp');
+
+$configurator
+    ->createRobotLoader()
+    ->register();
+
+$container = $configurator->createContainer();
+
+return $container;
