7c540af6c2f35a47552ba068f854a00158a2f32f
Nikolas Lada <nikolas.lada@gmail.com>
2020-02-22 12:26:27 +0100

    Added Definition class

---
+++ src/Domains/Definition.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Reasoner\Domains;
+
+class Definition implements  IDefinition
+{
+
+    /** @var int */
+    private $propertyId;
+
+    /** @var int */
+    private $typeOfRestriction;
+
+    /** @var null|int */
+    private $count;
+
+    /** @var null|Iset */
+    private $set;
+
+
+    public function __construct(int $propertyId, string $typeOfRestriction, ?int $count, ?Iset $set)
+    {
+        $this->propertyId = $propertyId;
+        $this->typeOfRestriction = $typeOfRestriction;
+        $this->count = $count;
+        $this->set = $set;
+    }
+
+    public function getPropertyId(): int
+    {
+        return $this->propertyId;
+    }
+
+    public function getTypeOfRestriction(): int
+    {
+        return $this->typeOfRestriction;
+    }
+
+    public function getCount(): ?int
+    {
+        return $this->count;
+    }
+
+    public function getSet(): ?ISet
+    {
+        return $this->set;
+    }
+
+}
