1e7ca6d8b28cfc49f963a0b9c67a4c1154f260f8
Nikolas Lada <nikolas.lada@gmail.com>
2020-02-25 23:07:42 +0100

    Added ReasonerModel class

---
+++ src/Models/ReasonerModel.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Reasoner\Reasoner\Models;
+
+
+class ReasonerModel
+{
+
+    public function getReasonedClassList(ClassTree $classTree, int ...$conditionClassId)
+    {
+    }
+
+}
